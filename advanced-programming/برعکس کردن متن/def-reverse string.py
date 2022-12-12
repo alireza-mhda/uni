7891f9baf4698b1a4282ffade40a96246d40d9fa
Alireza <102881491+alireza-mhda@users.noreply.github.com>
2022-12-12 15:58:24 +0330

    added print triangle

--- advanced-programming/برعکس کردن متن/def-reverse string.py
+++ advanced-programming/برعکس کردن متن/def-reverse string.py
@@ -1,22 +1,31 @@
+# Alireza Mohammadi-A
+# Sun. 10:10 ~ 12:25
+
 def reverse1(str):
   print(str[::-1])
 
-reverse1('amir')
-  
+
 def reverse2(str):
   reversed = ''
-  for i in range(len(str),0,-1):
+  for i in range(len(str), 0, -1):
     reversed += str[i-1]
   print(reversed)
-  
-reverse2('amir')
+
 
 def reverse3(str):
   reversed = ''
   i = len(str)
-  while(i > 0):
+  while (i > 0):
     reversed += str[i-1]
-    i-=1
+    i -= 1
   print(reversed)
-  
-reverse3('amir')    
+
+
+reverse1('amir')
+reverse2('amir')
+reverse3('amir')
+
+# Output
+"""
+rima
+"""
--- advanced-programming/تعداد کلمه ان حرفی/find n_words in a string.def.py
+++ advanced-programming/تعداد کلمه ان حرفی/find n_words in a string.def.py
@@ -1,8 +1,18 @@
-def find(str,n):
+# Alireza Mohammadi-A
+# Sun. 10:10 ~ 12:25
+
+def n_words(str, n):
   words = []
   for word in str.split(" "):
     if (len(word) == n):
       words.append(word)
   print("{} words have length {}:\n".format(len(words), n) + ", ".join(words))
 
-find("my name is amir and i love python", 4)
+
+n_words("my name is amir and i love python", 4)
+
+# Output
+"""
+3 words have length 4:
+name, amir, love
+"""
